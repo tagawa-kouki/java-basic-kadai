b295f78a304924846c61329dafb74289f59551ba
tagawa-kouki <122658663+tagawa-kouki@users.noreply.github.com>
2024-04-13 21:05:24 +0900

    課題8の提出

--- kadai_008/Price_Chapter08.java
+++ kadai_008/Price_Chapter08.java
@@ -4,29 +4,24 @@ public class Price_Chapter08 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		int age = 30; // 年代
+        int  userAge =  30; // 年代
+        String serviceCost = ""; // 料金
 
-		if( age < 10 ) {
-			System.out.println("上記以外の年代の料金は500円");
-		} else if( age < 20 ) {
-			System.out.println("10代の料金は1000円");
-		} else if( age < 30 ) {
-			System.out.println("20代の料金は2000円");
-		} else if(  age < 40 ) {
-			System.out.println("30代の料金は3000円");
-		} else if(  age < 50 ) {
-			System.out.println("40代の料金は3000円");
-		} else if(  age < 60 ) {
-			System.out.println("50代の料金は4000円");
-		} else if(  age < 70 ) {
-			System.out.println("60代の料金は4000円");
-		} else if(  age < 80 ) {
-			System.out.println("70代の料金は4000円");
-		} else if(  age < 90 ) {
-			System.out.println("80代の料金は5000円");
-		} else {
-			System.out.println("上記以外の年代の料金は500円");
-		}
+        // 年代に応じた料金を取得
+        serviceCost = switch(userAge) {
+            case 10  -> "10代の料金は1000円";
+            case 20  -> "20代の料金は2000円";
+            case 30  -> "30代の料金は3000円";
+            case 40  -> "40代の料金は3000円";
+            case 50  -> "50代の料金は4000円";
+            case 60  -> "60代の料金は4000円";
+            case 70  -> "70代の料金は4000円";
+            case 80  -> "80代の料金は5000円";
+            default -> "上記以外の年代の料金は500円";
+        };
+
+        // 料金を表示
+        System.out.println(serviceCost);
 	}
 
 }
