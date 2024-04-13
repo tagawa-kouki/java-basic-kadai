b031decfb64b3f5d6cafe45fab4c6be37796bb05
tagawa-kouki <122658663+tagawa-kouki@users.noreply.github.com>
2024-04-13 17:14:07 +0900

    課題8の提出

---
+++ kadai_008/Price_Chapter08.java
@@ -0,0 +1,32 @@
+package kadai_008;
+
+public class Price_Chapter08 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int age = 90; // 年代
+
+		if( age < 10 ) {
+			System.out.println("上記以外の年代の料金は500円");
+		} else if( age < 20 ) {
+			System.out.println("10代の料金は1000円");
+		} else if( age < 30 ) {
+			System.out.println("20代の料金は2000円");
+		} else if(  age < 40 ) {
+			System.out.println("30代の料金は3000円");
+		} else if(  age < 50 ) {
+			System.out.println("40代の料金は3000円");
+		} else if(  age < 60 ) {
+			System.out.println("50代の料金は3000円");
+		} else if(  age < 70 ) {
+			System.out.println("60代の料金は3000円");
+		} else if(  age < 80 ) {
+			System.out.println("70代の料金は3000円");
+		} else if(  age < 90 ) {
+			System.out.println("80代の料金は3000円");
+		} else {
+			System.out.println("上記以外の年代の料金は500円");
+		}
+	}
+
+}
